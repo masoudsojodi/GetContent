07bb6a4f93fac250bd824cd87c527bd22fe185d0
masoud.sojodi <masoud.sojodi@gmail.com>
2016-09-14 09:39:36 +0430

    cx

---
+++ index.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="GetContent.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+   <div runat="server" id="framein">
+    </div>
+  
+         <div runat="server" id="Div1">
+    </div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GetContent
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //sample1
+            Context.Response.Charset = Encoding.UTF8.WebName;
+            WebClient clientzonh = new WebClient();
+
+            string strclientzonh = clientzonh.DownloadString("http://www.zone-h.org/archive/special=1");
+            framein.InnerHtml = strclientzonh;
+
+
+
+
+
+            //sample2
+            WebRequest request3 = WebRequest.Create("http://iranfile.ir/search/showdetails.aspx?id=8009407");
+            WebResponse response3 = request3.GetResponse();
+            Stream data3 = response3.GetResponseStream();
+            string html3 = String.Empty;
+            using (StreamReader sr = new StreamReader(data3, Encoding.UTF8, true))
+            {
+                html3 = sr.ReadToEnd();
+            }
+
+            Div1.InnerHtml = html3;
+        }
+    }
+}
